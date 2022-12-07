65f7407c656491d3043274aa86f5c9c2517c3675
Treesa George <118190499+tresanotfound@users.noreply.github.com>
2022-12-07 15:42:15 +0530

    Update Calculator_python.py

--- Calculator_python.py
+++ Calculator_python.py
@@ -1,3 +1,6 @@
+
+
+
 # Program make a simple calculator
 
 def add(x, y):
@@ -12,27 +15,43 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
-print("MENU")
-print("Select operation:")
+def exponential(x,y):
+	return x**y
+
+def remainder(x,y):
+	return x%y
+	
+print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponential")
+print("6.Remainder)
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
-    if choice in ('1', '2', '3', '4'):
+    choice = input("Enter choice(1/2/3/4/5/6): ")
+    if choice in ('1', '2', '3', '4','5','6'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
+
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
+
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
+
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
+
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-      
+            print(num1, "/", num2, "=", divide(num1,num2))
+        
+        elif choice == '5':
+        	print(num1, "**", num2, "=", exponential(num1,num2))
+        
+        elif choice == '6':
+        	print(num1, "%", num2, "=", remainder(num1,num2))
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
